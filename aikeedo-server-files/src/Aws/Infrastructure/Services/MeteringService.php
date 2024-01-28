a5d22a225290905c183bee7daac5ad3167149311
m0th3rch1p <njagibret@gmail.com>
2024-01-29 00:43:47 +0300

    debug testing with us-east-1

--- aikeedo-server-files/src/Aws/Infrastructure/Services/MeteringService.php
+++ aikeedo-server-files/src/Aws/Infrastructure/Services/MeteringService.php
@@ -13,7 +13,7 @@ class MeteringService
     {
         $credentials = new Credentials(env('AWS_KEY'), env('AWS_SECRET'));
         $this->client = new MarketplaceMeteringClient([
-            'region' => 'us-east',
+            'region' => 'us-east-1',
             'version' => 'latest',
             'credentials' => $credentials
         ]);
