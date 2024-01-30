3b943a79e88457a2e0cb936ccfb35e3469441dc9
m0th3rch1p <njagibret@gmail.com>
2024-01-30 03:04:03 +0300

    sqs debug 7

--- aikeedo-server-files/src/Aws/Infrastructure/Services/SubscriptionSnsService.php
+++ aikeedo-server-files/src/Aws/Infrastructure/Services/SubscriptionSnsService.php
@@ -8,7 +8,7 @@ use Aws\Sns\SnsClient;
 class SubscriptionSnsService
 {
     private SnsClient $client;
-    private string $topicArn = "arn:aws:sns:us-east-1:287250355862:aws-mp-subscription-notification-2lqek63x8ixl9bu07a3dkw96v";
+    private string $topicArn = "arn:aws:sns:us-east-1:287250355862:aws-mp-subscription-notification-1cothn9ewdy8kts24xi9fre3y";
     private string $endpoint = "arn:aws:sqs:us-east-1:436917423698:chatrov2";
 
     public function __construct()
